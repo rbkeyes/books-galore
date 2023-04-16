ff0cdbaa3fb2fcd35cd1b75b3c177569ed4fbb05
Reed Keyes <rbkeyes@gmail.com>
2023-04-16 14:09:47 -0700

    delete book using mutation is working as expected

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -73,7 +73,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    console.log(bookToSave);
+    console.log(bookToSave)
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -93,6 +93,8 @@ const SearchBooks = () => {
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      console.log(savedBookIds)
+
     } catch (err) {
       console.error(err);
     }
